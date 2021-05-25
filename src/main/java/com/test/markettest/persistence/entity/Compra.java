024eca259e139c9d7d797d0f5b7a75ab822fd678
Carlos Bello <bellocarlos2810@gmail.com>
2021-05-24 22:23:11 -0500

    create Entitys

--- src/main/java/com/test/markettest/persistence/entity/Compra.java
+++ src/main/java/com/test/markettest/persistence/entity/Compra.java
@@ -1,2 +1,79 @@
-package com.test.markettest.persistence.entity;public class Compra {
+package com.test.markettest.persistence.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @Column(name = "id_compra")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private Character medioPago;
+
+    private String comentario;
+
+    private Character estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
+    private Cliente cliente;
+
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public Character getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(Character medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Character getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Character estado) {
+        this.estado = estado;
+    }
 }
--- src/main/java/com/test/markettest/persistence/entity/CompraProducto.java
+++ src/main/java/com/test/markettest/persistence/entity/CompraProducto.java
@@ -1,2 +1,57 @@
-package com.test.markettest.persistence.entity;public class CompraProducto {
+package com.test.markettest.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "compas_productos")
+public class CompraProducto {
+
+    @EmbeddedId
+    private CompraProductoPK id;
+
+    private Integer cantidad;
+
+    private  Integer Total;
+
+    private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra", insertable = false, updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", insertable = false, updatable = false)
+    private Producto producto;
+
+    public CompraProductoPK getId() {
+        return id;
+    }
+
+    public void setId(CompraProductoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Integer getTotal() {
+        return Total;
+    }
+
+    public void setTotal(Integer total) {
+        Total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
